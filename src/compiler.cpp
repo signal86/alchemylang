4ba92c74a30d33f48e2952d2238d962cafc6e092
signal86 <diafakemail@gmail.com>
2024-05-05 20:44:09 -0500

    architecture blocks and comment syntax checking

--- src/compiler.cpp
+++ src/compiler.cpp
@@ -53,8 +53,8 @@ class ErrorHandler {
 bool lexer(std::string fileName, std::vector<std::string> *data) {
     std::vector<std::string> dataCopy = *data;
     ErrorHandler errors("lexizer/syntax error", fileName);
-    // errors.addError(3, "symbol not found", "example line one");
 
+    int blockNum = 0;
     std::string architecture = "none";
     for (auto i = dataCopy.begin(); i != dataCopy.end(); i++) {
 
@@ -72,10 +72,62 @@ bool lexer(std::string fileName, std::vector<std::string> *data) {
             wordList.push_back(splitToken);
         }
 
-        // // dump lines to terminal
+        // loop all words in the line and search through word values
+        for (int v = 0; v < wordList.size(); v++) {
+            // architecture blocks
+            if (wordList[v] == "meta" || wordList[v] == "view" || wordList[v] == "components" || wordList[v] == "global_modifiers") {
+                if (architecture == "none") architecture = wordList[v];
+                else errors.addError(std::distance(dataCopy.begin(), i) + 1, "overlapping architecture blocks", line);
+                int tempBlockNum = blockNum;
+                bool found = false;
+                // next character is a {
+                if (v + 1 < wordList.size() && wordList[v + 1] == "{") {
+                    // reverse through all lines to find the opposite "}"
+                    for (int j = dataCopy.size() - 1; j > 0; j--) {
+                        // redo separation of lines into words
+                        std::string line2 = dataCopy[j];
+                        std::string splitToken2;
+                        std::stringstream lineStream2(line2);
+                        std::vector<std::string> wordList2;
+                        while (getline(lineStream2, splitToken2, ' ')) {
+                            wordList2.push_back(splitToken2);
+                        }
+
+                        // iterate through the words
+                        for (int k = 0; k < wordList2.size(); k++) {
+                            if (wordList2[k] == "}") {
+                                if (tempBlockNum > 0) tempBlockNum--;
+                                else {
+                                    blockNum++;
+                                    found = true;
+                                }
+                            }
+                        }
+                    }
+                }
+                // architecture block never opend
+                else errors.addError(std::distance(dataCopy.begin(), i) + 1, "architecture block not opened", line);
+                if (!found) errors.addError(std::distance(dataCopy.begin(), i) + 1, "architecture block left incomplete", line);
+            }
+
+            else if (wordList[v] == "}") {
+                if (architecture == "none") errors.addError(std::distance(dataCopy.begin(), i) + 1, "architecture block not found", line);
+                else {
+                    architecture = "none";
+                    blockNum--;
+                }
+            }
+
+            else if (wordList[v] == "//") {
+                v = wordList.size() - 1;
+            }
+        }
+
+        // // dump words on line to terminal
         // for (int i = 0; i < wordList.size(); i++) {
         //     std::cout << ">" << wordList[i] << "<" << std::endl;
         // }
+        // // dump lines to terminal
         // std::cout << "line " << std::distance(dataCopy.begin(), i) + 1 << ": " << *i << "\n";
     }
 
